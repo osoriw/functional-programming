7989df030f6245385e7b26f15da23d706b4345b0
waosorio2021 <89051791+waosorio2021@users.noreply.github.com>
2023-01-10 13:36:35 -0500

    Update FunctionalProgrammingApplication.java
    Adding CommandLineRunner interface.

--- src/main/java/org/waosorio/functionalprogramming/FunctionalProgrammingApplication.java
+++ src/main/java/org/waosorio/functionalprogramming/FunctionalProgrammingApplication.java
@@ -1,13 +1,18 @@
 package org.waosorio.functionalprogramming;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FunctionalProgrammingApplication {
+public class FunctionalProgrammingApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FunctionalProgrammingApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
 }
