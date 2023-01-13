5f91c6a2c2b7258b75d11bdc14bd6d383a0b8e14
waosorio2021 <89051791+waosorio2021@users.noreply.github.com>
2023-01-13 12:12:08 -0500

    Agregando comentario
    Agregando comentario.

--- src/main/java/org/waosorio/functionalprogramming/functions/TestFunction.java
+++ src/main/java/org/waosorio/functionalprogramming/functions/TestFunction.java
@@ -5,6 +5,7 @@ import java.util.function.Function;
 public class TestFunction {
 
     public static void main(String[] args) {
+        /*****INTERFAZ FUNCIONAL Function*****/
         Function<Integer, String> converter = param -> Integer.toString(param);
         System.out.println(converter.apply(100).length());
     }
