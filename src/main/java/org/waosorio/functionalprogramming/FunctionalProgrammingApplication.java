b6c7733db64841f54ba1f64a06893cff2c0eadd1
waosorio2021 <89051791+waosorio2021@users.noreply.github.com>
2023-01-13 12:10:53 -0500

    Interface Function
    Implementando Interfaz funcional Function.

--- src/main/java/org/waosorio/functionalprogramming/FunctionalProgrammingApplication.java
+++ src/main/java/org/waosorio/functionalprogramming/FunctionalProgrammingApplication.java
@@ -7,12 +7,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FunctionalProgrammingApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FunctionalProgrammingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionalProgrammingApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-	}
+    @Override
+    public void run(String... args) throws Exception {
+
+        /**
+         * INTERFFACES FUNCIONALES MAS IMPORTANTES DISPONIBLES A PARTIR DE LA VERSION 8 DE JAVA
+         */
+
+        /**
+         * ANOTACIONES IMPORTANTES ACERCA DE LAS INTERFACES FUNCIONALES:
+         * -Una interfaz funcional se puede anotar con @FunctionalInterface, esto es una buena práctica, ya que nos ayuda
+         *  a identificar si la interfaz funcional que estamos anotando, cumple con los requerimientos para ser una interfaz funcional.
+         * -Las interfaces funcionales que vienen por defecto a partir de la versión 8 de java, se encuentran en el paquete
+         *  java.util.function.
+         * -
+         */
+
+    }
 }
---
+++ src/main/java/org/waosorio/functionalprogramming/functions/TestFunction.java
@@ -0,0 +1,11 @@
+package org.waosorio.functionalprogramming.functions;
+
+import java.util.function.Function;
+
+public class TestFunction {
+
+    public static void main(String[] args) {
+        Function<Integer, String> converter = param -> Integer.toString(param);
+        System.out.println(converter.apply(100).length());
+    }
+}
