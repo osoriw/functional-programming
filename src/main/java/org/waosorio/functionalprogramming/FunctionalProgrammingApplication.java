6689eca2f818529e748c349720446f033ed3e6ef
waosorio2021 <89051791+waosorio2021@users.noreply.github.com>
2023-03-26 14:55:56 -0500

    Creando Streams a partir de valores
    Ejemplos para mostrar como se pueden crear Streams a partir de diferentes tipos de valores, como Strings, arreglos, valores numéricos, usando el método of.

--- src/main/java/org/waosorio/functionalprogramming/FunctionalProgrammingApplication.java
+++ src/main/java/org/waosorio/functionalprogramming/FunctionalProgrammingApplication.java
@@ -3,16 +3,19 @@ package org.waosorio.functionalprogramming;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.waosorio.functionalprogramming.pojos.Estudiante;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class FunctionalProgrammingApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FunctionalProgrammingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionalProgrammingApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-	}
+    @Override
+    public void run(String... args) throws Exception {
+    }
 }
