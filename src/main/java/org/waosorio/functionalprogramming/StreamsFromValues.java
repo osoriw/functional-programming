067b090699d5746e378bcbb2cea1155ad2f14463
waosorio2021 <89051791+waosorio2021@users.noreply.github.com>
2023-07-19 22:41:30 -0500

    Renaming some classes
    Renaming some classes.

--- src/main/java/org/waosorio/functionalprogramming/StreamsFromValues.java
+++ src/main/java/org/waosorio/functionalprogramming/StreamsFromValues.java
@@ -2,35 +2,27 @@ package org.waosorio.functionalprogramming;
 
 import org.waosorio.functionalprogramming.pojos.Estudiante;
 
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-/**
- * Lógica que muestra como crear Streams a partir de diferentes tipos de valores.
- */
 public class StreamsFromValues {
+
     public static void main(String[] args) {
-        //Creando un Stream a partir de un valor
-        Stream<String> stream1 = Stream.of("String1", "String2", "String3", "String4");
-        //stream1.forEach(n -> System.out.println(n));// usando una expresión lamnbda
-        stream1.forEach(System.out::println);//usando un método referenciado
-        System.out.println("\n");
+        //Creando un stream de un sólo elemento
+        Stream<String> string = Stream.of("La programacion funcional es divertida!!\n");
+        string.forEach(System.out::println);
 
-        //Creando un Stream a partir de un arreglo
-        String[] array = {"java", "C++", "ruby", "python"};
-        Stream<String> stream2 = Stream.of(array);
-        stream2.forEach(System.out::println);
-        System.out.println("\n");
+        //Creando un stream de más de un elemento
+        Stream<String> strings = Stream.of("Hola", "mundo", "java\n");
+        strings.forEach(System.out::println);
 
-        //Creando un Stream a partir de un objeto
-        Stream<Estudiante> streamEstudiantes = Stream.<Estudiante>builder().add(new Estudiante("13456", 22, 1.62, 3.8)).add(new Estudiante("56166", 18, 1.75, 4.2)).add(new Estudiante("56165", 19, 1.56, 3.5)).build();
-        streamEstudiantes.forEach(estudiante -> System.out.println(estudiante.getIdentificacion()));
-        System.out.println("\n");
+        //Creando un stream a partir de la operación of, recibiendo como parámetro un arreglo
+        String[] lenguajesArr = {"java", "ruby", "node", "c++", "c#\n"};
+        Stream<String> lenguajesStrm = Stream.of(lenguajesArr);
+        lenguajesStrm.forEach(System.out::println);
 
-        //Creando un Stream de valores númericos y rango
-        //IntStream unoAVeinte = IntStream.range(1,21);//el último número no se incluye dentro del rango
-        IntStream unoAVeinte = IntStream.rangeClosed(1, 20);//el último número si se incluye dentro del rango
-        unoAVeinte.forEach(System.out::println);
-        System.out.println("\n");
+        //Creando un stream con el uso del método builder
+        Stream<Estudiante> streamEsts = Stream.<Estudiante>builder().add(new Estudiante("n01", 17, 1.70, 9.5)).add(new Estudiante("n02", 25, 1.65, 9.0)).add(new Estudiante("n03", 19, 1.86, 837)).build();
+
+        streamEsts.forEach(est -> System.out.println(est.getIdentificacion()));
     }
 }
---
+++ src/main/java/org/waosorio/functionalprogramming/StreamsFromValues1.java
@@ -0,0 +1,36 @@
+package org.waosorio.functionalprogramming;
+
+import org.waosorio.functionalprogramming.pojos.Estudiante;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Lógica que muestra como crear Streams a partir de diferentes tipos de valores.
+ */
+public class StreamsFromValues1 {
+    public static void main(String[] args) {
+        //Creando un Stream a partir de un valor
+        Stream<String> stream1 = Stream.of("String1", "String2", "String3", "String4");
+        //stream1.forEach(n -> System.out.println(n));// usando una expresión lamnbda
+        stream1.forEach(System.out::println);//usando un método referenciado
+        System.out.println("\n");
+
+        //Creando un Stream a partir de un arreglo
+        String[] array = {"java", "C++", "ruby", "python"};
+        Stream<String> stream2 = Stream.of(array);
+        stream2.forEach(System.out::println);
+        System.out.println("\n");
+
+        //Creando un Stream a partir de un objeto
+        Stream<Estudiante> streamEstudiantes = Stream.<Estudiante>builder().add(new Estudiante("13456", 22, 1.62, 3.8)).add(new Estudiante("56166", 18, 1.75, 4.2)).add(new Estudiante("56165", 19, 1.56, 3.5)).build();
+        streamEstudiantes.forEach(estudiante -> System.out.println(estudiante.getIdentificacion()));
+        System.out.println("\n");
+
+        //Creando un Stream de valores númericos y rango
+        //IntStream unoAVeinte = IntStream.range(1,21);//el último número no se incluye dentro del rango
+        IntStream unoAVeinte = IntStream.rangeClosed(1, 20);//el último número si se incluye dentro del rango
+        unoAVeinte.forEach(System.out::println);
+        System.out.println("\n");
+    }
+}
